2b1ab413ea41e868e646e21382c2122edf4dc120
Ann Tseng <anntseng@Anns-MacBook-Pro.local>
2023-10-29 22:23:13 -0700

    new beginning

--- assets/js/app.js
+++
@@ -1,7 +0,0 @@
-// Nav hamburgerburger selections
-const burger = document.querySelector("#burger-menu");
-const ul = document.querySelector("nav ul");
-const nav = document.querySelector("nav");
-burger.addEventListener("click", () => {
-    ul.classList.toggle("show");
-});
